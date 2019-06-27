cd966e06b28512d7a6c8fa42b5df8c3a19af4876
SenpaiPotato <minrulee8@gmail.com>
2019-06-27 13:33:32 -0400

    finally

---
+++ data/survey/survey.py
@@ -0,0 +1,8 @@
+dict_data = {}
+user_name = input("Type your name: ")
+dict_data['name'] = user_name
+user_age = input("how old are you? ")
+dict_data['age'] = user_age
+user_city = input("What city are you from? ")
+dict_data['city'] = user_city
+print(dict_data)
